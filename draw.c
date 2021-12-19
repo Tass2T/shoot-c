0fa673722ef14e49d58f9cecb645c29706bc605b
tass2t <brunog90@gmail.com>
2021-12-19 23:14:18 +0100

    added 60 fps limiter

--- draw.c
+++ draw.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 void drawRectangle () {
-    
-    SDL_Rect rect1 = {10, 23, 50, 60};
     SDL_RenderClear(pRenderer);
-    
-    SDL_SetRenderDrawColor(pRenderer, 25, 250, 10, 255);
-    SDL_RenderDrawRect(pRenderer, &rect1);
+    SDL_SetRenderDrawColor(pRenderer, 145,13,0,255);
+    SDL_RenderFillRect(pRenderer, &rect1);
+    SDL_SetRenderDrawColor(pRenderer, 30, 125,10,255);
+    SDL_RenderFillRect(pRenderer, &rect2);
     SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
 
     SDL_RenderPresent(pRenderer);
+
 }
--- input.c
+++ input.c
@@ -3,16 +3,38 @@
 void getInput()
 {
     SDL_Event events;
+    SDL_Rect collisionRectangle;
 
     while (SDL_PollEvent(&events))
+    {
+        switch(events.type)
         {
-            switch(events.type)
-            {
-                case SDL_QUIT:
-                    isOpen = false;
-                    break;
+            case SDL_QUIT:
+                isOpen = false;
+                break;
+            case SDL_KEYDOWN:
+                switch (events.key.keysym.scancode)
+                {
+                    case SDL_SCANCODE_RIGHT:
+                        rect1.x = rect1.x + SPEED;
+                        if (SDL_IntersectRect(&rect1, &rect2,&collisionRectangle)) SDL_Log("collision here");
+                        break;
+                    case SDL_SCANCODE_LEFT:
+                        rect1.x = rect1.x - SPEED;
+                        break;
+                    case SDL_SCANCODE_DOWN:
+                        rect1.y = rect1.y + SPEED;
+                        break;
+                    case SDL_SCANCODE_UP:
+                        rect1.y = rect1.y -SPEED;
+                        break;
+                    case SDL_SCANCODE_A:
+                        isOpen = false;
+                    default:
+                        printf("Physical %s key acting as %s key", SDL_GetScancodeName(events.key.keysym.scancode), SDL_GetKeyName(events.key.keysym.sym));
+                        break;
+                }
+                break;
             }
-
         }
-
-}
+    }
